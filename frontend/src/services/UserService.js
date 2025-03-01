8613dbfaf0952f7291c91e2bf5e8775b90859103
Eli Rudman <erudman21@gmail.com>
2025-03-01 12:31:23 -0800

    add url change

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const USER_API_BASE_URL = "http://localhost:9080/users";
+const getBaseUrl = () => {
+    return typeof window !== 'undefined' ? '/api' : 'http://backend:9080';
+};
+  
+const USER_API_BASE_URL = `${getBaseUrl()}/users`;
 
 class UserService {
 
