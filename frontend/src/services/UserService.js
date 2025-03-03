8f6c095b28ca9370e9c0cf51c97ce63952303bfa
Eli Rudman <erudman21@gmail.com>
2025-03-02 21:15:07 -0800

    undo change

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -9,7 +9,7 @@ class UserService {
     }
 
     createUser(user){
-        return axios.post(USER_API_BASE_URL + '123', user);
+        return axios.post(USER_API_BASE_URL, user);
     }
 
     getUserById(userId){
