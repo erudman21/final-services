103f1220236b9ccf5813e1510e2980f6ed5bce0c
Eli Rudman <erudman21@gmail.com>
2025-03-02 21:06:34 -0800

    changed url

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -9,7 +9,7 @@ class UserService {
     }
 
     createUser(user){
-        return axios.post(USER_API_BASE_URL, user);
+        return axios.post(USER_API_BASE_URL + '123', user);
     }
 
     getUserById(userId){
